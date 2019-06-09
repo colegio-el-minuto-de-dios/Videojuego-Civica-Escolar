8ae64ca0cd1ef32c561a600946c15babbf374a77
Andrés Sarmiento <acamilo707@gmail.com>
2019-06-09 11:04:32 -0500

    Se a agrega portada al menú

---
+++ Videojuego-Civica-Escolar/Assets/Scripts/GUI/abrirMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class abrirMenu : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            Animator animator = Panel.GetComponent<Animator>();
+            if (animator != null)
+            {
+                bool isOpen = animator.GetBool("abrir");
+                animator.SetBool("abrir", !isOpen);
+            } 
+        }
+    }
+}
