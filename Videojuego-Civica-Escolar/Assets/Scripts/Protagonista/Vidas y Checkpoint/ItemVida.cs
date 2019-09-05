3e3ff25dbc1fde6b206cba4e9165bca528a3cfa4
ACSG-64 <acamilo707@gmail.com>
2019-09-05 16:08:34 -0500

    Pantalla de perdida, panntalla de carva (no está terminada) y sistema de vidas

---
+++ Videojuego-Civica-Escolar/Assets/Scripts/Protagonista/Vidas y Checkpoint/ItemVida.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemVida : MonoBehaviour
+{
+    public int salud;
+    public float velocidad;
+    // Start is called before the first frame update
+
+    void Update(){
+        transform.Rotate(new Vector3(0f, velocidad, 0f) * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player"){
+            sistemaVidas.salud += salud;
+            Destroy(gameObject);
+            print("");
+        }
+        
+    }
+
+}
