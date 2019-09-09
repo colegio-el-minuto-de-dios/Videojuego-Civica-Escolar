eea92e19fe47741dc0ed4e67b544ba9c0ed64b5b
Andrés Sarmiento <acamilo707@gmail.com>
2019-09-09 00:32:13 -0500

    Fase primaria selector de islas

---
+++ Videojuego-Civica-Escolar/Assets/Scripts/GUI/DIALOGOS INESTABLE/DialogueManagerGeneral.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+
+public class DialogueManagerGeneral : MonoBehaviour
+{
+
+	public TextMeshProUGUI nameText;
+	public TextMeshProUGUI dialogueText;
+
+	public Animator animator;
+
+	public Animator NpcAnim;
+
+	private Queue<string> sentences;
+
+	public GameObject jugador;
+	public GameObject camaraJugador;
+ 
+
+
+	// Use this for initialization
+	void Start () {
+		sentences = new Queue<string>();
+		
+	}
+
+	public void StartDialogue (Dialogue dialogue)
+	{
+		camaraJugador.GetComponent<CameraFollow>().enabled = false;
+		jugador.GetComponent<controlPersonaje>().enabled = false;
+
+		NpcAnim.SetBool("estaHablando", true);
+		animator.SetBool("IsOpen", true);
+
+		nameText.text = dialogue.name;
+
+		sentences.Clear();
+
+		foreach (string sentence in dialogue.sentences)
+		{
+			sentences.Enqueue(sentence);
+		}
+
+		DisplayNextSentence();
+	}
+
+	public void DisplayNextSentence ()
+	{
+
+		if (sentences.Count == 0)
+		{
+			EndDialogue();
+			return;
+		}
+
+		string sentence = sentences.Dequeue();
+		StopAllCoroutines();
+		StartCoroutine(TypeSentence(sentence));
+	}
+
+	IEnumerator TypeSentence (string sentence)
+	{
+		dialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray())
+		{
+			dialogueText.text += letter;
+			yield return null;
+		}
+	}
+
+	public void EndDialogue()
+	{
+		camaraJugador.GetComponent<CameraFollow>().enabled = true;
+		jugador.GetComponent<controlPersonaje>().enabled = true;
+		animator.SetBool("IsOpen", false);
+		NpcAnim.SetBool("estaHablando", false);
+	}
+
+    
+
+}
---
+++ Videojuego-Civica-Escolar/Assets/Scripts/GUI/SelectorIslas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectorIslas : MonoBehaviour
+{
+    public GameObject dialogos;
+    public Animator fondoSolido;
+    public GameObject habilitarPanel;
+    public GameObject camaraJugador;
+    public GameObject camaraPanel;
+    public void MostrarMapa(){
+        
+        dialogos.SetActive(false);
+        fondoSolido.SetBool("abierto", true);
+        camaraJugador.SetActive(false);
+        camaraPanel.SetActive(true);
+        habilitarPanel.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+        
+    }
+
+    public void CerrarMapa(){
+        dialogos.SetActive(true);
+        fondoSolido.SetBool("abierto", false);
+        camaraJugador.SetActive(true);
+        camaraPanel.SetActive(false);
+        habilitarPanel.SetActive(true);
+        FindObjectOfType<DialogueManagerGeneral>().DisplayNextSentence();
+    }
+}
