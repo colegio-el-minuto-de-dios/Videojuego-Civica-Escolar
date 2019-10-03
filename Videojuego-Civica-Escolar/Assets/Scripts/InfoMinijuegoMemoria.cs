6b382e4e139d9fd386d77014d4ab271561e90271
Andrés Sarmiento <acamilo707@gmail.com>
2019-10-03 03:37:44 -0500

    Isla biblioteca con limitantes e instrucciones. El controlador de animación para el jugador fué actualizado y se agregan reemplazos temporales para el esqueleto.

---
+++ Videojuego-Civica-Escolar/Assets/Scripts/InfoMinijuegoMemoria.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoMinijuegoMemoria : MonoBehaviour
+{
+    public GameObject cubos;
+    public GameObject instruccion;
+    public GameObject camaraA;
+    public GameObject camaraB;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubos.SetActive(false);
+        instruccion.SetActive(false);  
+        camaraA.SetActive(false);
+        camaraB.SetActive(false);       
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter(Collider other){
+        if(other.tag == "Player"){
+            cubos.SetActive(true);
+            instruccion.SetActive(true); 
+            if(gameObject.name == "Cubo A"){                   
+                camaraA.SetActive(true);                 
+            }
+            if(gameObject.name == "Cubo B"){                   
+                camaraB.SetActive(true);                 
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other){
+        if(other.tag == "Player"){
+            cubos.SetActive(false);
+            instruccion.SetActive(false);  
+            camaraA.SetActive(false);
+            camaraB.SetActive(false);  
+        }
+    }
+}
